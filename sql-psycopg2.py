6a01c98655432e73593766a9f330be28eb0d0869
Tomislav Dukez <tomdu3@ymail.com>
2023-07-06 13:09:25 +0000

    add sql test file

---
+++ sql-psycopg2.py
@@ -0,0 +1,37 @@
+import psycopg2
+
+
+#connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select all tracks where the composer is "Queen" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the result (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+
+for result in results:
+    print(result)
